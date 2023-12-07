8c3c704b16455190cd4d456febe8c31480bbe4ea
Michael Jensen <mjensen@riotgames.com>
2023-12-07 09:33:01 -0800

    ignore out files

--- out/template.js
+++
@@ -1,7 +0,0 @@
-var tfs = require('fs');
-require('colors');
-// Usage: node index.js inputFile
-var tfile = process.argv[2];
-var tinput = fs.readFileSync(file, 'utf8').split('\n').filter(function (i) { return i; });
-//input contains entry per line in inputFile
-//# sourceMappingURL=template.js.map
